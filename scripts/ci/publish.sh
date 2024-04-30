715ad08ec2899de122d9df6197180a2cedd6bdf3
kirinnee <kirinnee97@gmail.com>
2024-04-30 18:35:07 +0800

    feat: update resume

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -3,5 +3,5 @@
 set -eouf pipefail
 xelatex resume.tex
 
-mc alias set personal "$HOST" "$S3_ID" "$3_SECRET"
+mc alias set personal "$S3_HOST" "$S3_ID" "$3_SECRET"
 mc cp ./resume.pdf personal/personal/resume.pdf
