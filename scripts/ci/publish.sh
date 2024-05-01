7832357b754fc33a689dab4aeeaa18a8f21c54c3
kirinnee <kirinnee97@gmail.com>
2024-05-01 20:15:47 +0800

    ci: redirect latex build to devnull to see stderr

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -eouf pipefail
-xelatex resume.tex
+xelatex resume.tex >"/dev/null"
 
 mc alias set personal "$S3_HOST" "$S3_ID" "$3_SECRET"
 mc cp ./resume.pdf personal/personal/resume.pdf
