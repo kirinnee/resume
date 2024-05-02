d336e1c96dd5ebc4c16151a745f2cf74a457fd2b
kirinnee <kirinnee97@gmail.com>
2024-05-02 11:01:33 +0800

    fix: ci pipeline

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+s3host="$S3_HOST"
+s3id="$S3_ID"
+s3secret="$S3_SECRET"
+[ "$s3host" = '' ] && echo "S3_HOST not found" && exit 1
+[ "$s3id" = '' ] && echo "S3_ID not found" && exit 1
+[ "$s3secret" = '' ] && echo "S3_SECRET not found" && exit 1
+
 set -eouf pipefail
 xelatex resume.tex >"/dev/null"
 
-mc alias set personal "$S3_HOST" "$S3_ID" "$3_SECRET"
+mc alias set personal "$s3host" "$s3id" "$s3secret"
 mc cp ./resume.pdf personal/personal/resume.pdf
